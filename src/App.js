ae307f3b64054c013b754ef7df5143bf51492e6a
Morris Alromhein <Morris4028@gmail.com>
2023-05-07 22:08:50 -0400

    Added colored circles

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import React from "react";
 import Tree from "react-d3-tree";
-import orgChartJson from "./data/names.json";
 import closeChartJson from "./data/close.json";
 import { useCenteredTree } from "./helpers";
 import "./styles.css";
 
 const containerStyles = {
   width: "100vw",
-  height: "100vh"
+  height: "100vh",
 };
 
 // Here we're using `renderCustomNodeElement` to represent each node
 // as an SVG `rect` instead of the default `circle`.
 const renderRectSvgNode = ({ nodeDatum, toggleNode }) => (
   <g>
-    <circle r="10" onClick={toggleNode} />
-    <text fill="black" strokeWidth="1" x="20">
-      {nodeDatum.name}
-    </text>
-    {nodeDatum.attributes?.department && (
-      <text fill="black" x="20" dy="20" strokeWidth="1">
-        Department: {nodeDatum.attributes?.department}
+    <circle r="10" onClick={toggleNode} fill="" />
+    {nodeDatum.gender === "Male" && (
+      <>
+    <circle r="10" onClick={toggleNode} fill="lightblue" />
+      <text fill="black" strokeWidth="1" x="20">
+        {nodeDatum.name}
       </text>
+      </>
+    )}
+    {nodeDatum.gender === "Female" && (
+      <>
+    <circle r="10" onClick={toggleNode} fill="lightpink" />
+      <text fill="black" strokeWidth="1" x="20">
+        {nodeDatum.name}
+      </text>
+      </>
     )}
   </g>
 );
