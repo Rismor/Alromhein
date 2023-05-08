e9fc29eef0bb67227467decf673cad67238cba0d
Morris Alromhein <Morris4028@gmail.com>
2023-05-07 23:38:15 -0400

    Changed Title + Added Icon

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { Helmet } from "react-helmet";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
+    <Helmet>
+      <title> Romhen Family Tree </title>
+    </Helmet>
     <App />
   </StrictMode>,
   rootElement
